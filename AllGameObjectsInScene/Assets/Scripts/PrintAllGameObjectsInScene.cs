2e4efa6fd6a27bb50615afafdf36145d1f805f50
maxidea1024@gmail.com <maxidea1024@gmail.com>
2020-02-07 10:04:05 +0900

    Add files via upload

---
+++ AllGameObjectsInScene/Assets/Scripts/PrintAllGameObjectsInScene.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PrintAllGameObjectsInScene : MonoBehaviour
+{
+    void Start()
+    {
+        var scene = SceneManager.GetActiveScene();
+
+        var list = new List<GameObject>();
+        SceneUtil.CollectAllGameObjectInScene(scene, list);
+
+        for (int i = 0, n = list.Count; i < n; ++i)
+        {
+            var go = list[i];
+
+            Debug.LogFormat("{0:D04} : ( NamePath : \"{1}\", InstanceID : {2:X} )", i + 1, SceneUtil.GetGameObjectPath(go), go.GetInstanceID());
+        }
+    }
+}
---
+++ AllGameObjectsInScene/Assets/Scripts/SceneUtil.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public static class SceneUtil
+{
+    public static int CollectAllGameObjectInScene(Scene scene, List<GameObject> outGameObjects)
+    {
+        outGameObjects.Clear(); // just in time
+
+        var rootGameObjects = new List<GameObject>();
+        scene.GetRootGameObjects(rootGameObjects);
+        
+        for (int i = 0, n = rootGameObjects.Count; i < n; ++i)
+        {
+            CollectChildren(rootGameObjects[i].transform, outGameObjects);
+        }
+        
+        return outGameObjects.Count;
+    }
+
+    private static void CollectChildren(Transform parent, List<GameObject> list)
+    {
+        list.Add(parent.gameObject);
+
+        for (int i = 0, n = parent.childCount; i < n; ++i)
+        {
+            CollectChildren(parent.GetChild(i), list);
+        }
+    }
+
+    public static string GetGameObjectPath(GameObject go)
+    {
+        string path = go.name;
+
+        Transform node = go.transform.parent;
+        while (node != null)
+        {
+            path = node.gameObject.name + "/" + path;
+            node = node.parent;
+        }
+
+        path = "/" + path;
+
+        return path;
+    }
+}
